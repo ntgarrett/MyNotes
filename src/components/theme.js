c772a68ca088cb8b1a9003302ebbe7231837b3c0
Nick Garrett <garrett.nick.96@gmail.com>
2021-08-07 13:23:45 -0700

    Adds themes

---
+++ src/components/theme.js
@@ -0,0 +1,11 @@
+export const darkTheme = {
+  background: '#121212',
+  foreground: '#1f1f1f',
+  text: '#e0e0e0'
+};
+
+export const lightTheme = {
+  background: '#ffffff',
+  foreground: '#bdbdbd',
+  text: '#000000'
+};
