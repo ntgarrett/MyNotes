7ea02e4d54afaba5f503d0f678379a8fc6db7990
Nick Garrett <garrett.nick.96@gmail.com>
2021-08-09 12:51:47 -0700

    Adjusted theme color value

--- src/components/theme.js
+++ src/components/theme.js
@@ -1,11 +1,11 @@
 export const darkTheme = {
   background: '#121212',
   foreground: '#1f1f1f',
-  text: '#e0e0e0'
+  text: '#e0e0e0',
 };
 
 export const lightTheme = {
   background: '#ffffff',
-  foreground: '#bdbdbd',
+  foreground: '#e5e5e5',
   text: '#000000'
 };
